144264144d3b4582c062a1a559955f64ff1e974d
Lawrence Cherone <vripscript@gmail.com>
2015-10-09 15:52:22 +0000

    Initial commit

---
+++ src/Cron.php
@@ -0,0 +1,96 @@
+<?php
+namespace plinker\Cron {
+
+    use RedBeanPHP\R;
+
+    class Cron {
+
+        public $config = array();
+        private $tab;
+
+        public function __construct(array $config = array(
+            'config' => array(
+                'taskfile' => './cron-task-file',
+                'applyCrontab' => false
+            ),
+        )) {
+            $this->config = $config;
+
+            //check database construct values
+            if (empty($this->config['config'])) {
+                exit(json_encode($this->response(
+                    'Bad Request',
+                    400,
+                    array('config construct error [database] empty')
+                ), JSON_PRETTY_PRINT));
+            } else {
+                $this->tab = new lib\CronFileWriter($this->config['config']['taskfile']);
+            }
+        }
+
+        private function response($data = null, $status = 200, $errors = array())
+        {
+            return array(
+                'status' => $status,
+                'errors' => $errors,
+                'data'   => $data
+            );
+        }
+
+        public function crontab()
+        {
+            return $this->dump();
+        }
+
+        public function applyCrontab()
+        {
+            if (!empty($this->config['config']['applyCrontab'])) {
+                return exec('crontab '.$this->config['config']['taskfile']);
+            }
+            return false;
+        }
+
+        public function create(array $params = array())
+        {
+            $key = $params[0];
+            $value = $params[1];
+            $this->tab->create($key, $value);
+        }
+
+        public function read(array $params = array())
+        {
+            $key = $params[0];
+            return $this->tab->read($key);
+        }
+
+        public function update(array $params = array())
+        {
+            $key = $params[0];
+            $value = $params[1];
+            $this->tab->update($key, $value);
+
+        }
+
+        public function delete(array $params = array())
+        {
+            $key = $params[0];
+            $this->tab->delete($key);
+        }
+
+        public function drop(array $params = array())
+        {
+            $this->tab->drop();
+        }
+
+        public function dump(array $params = array())
+        {
+            return $this->tab->dump();
+        }
+
+        public function __destruct()
+        {
+            $this->applyCrontab();
+        }
+    }
+
+}
---
+++ src/lib/CronFileWriter.php
@@ -0,0 +1,153 @@
+<?php
+namespace plinker\Cron\lib;
+
+/**
+ * Flatfile CRUD class
+ *
+ * @author Lawrence Cherone
+ * @version 1.01
+ */
+class CronFileWriter
+{
+    public $file;
+    private $handle;
+
+    /**
+	 * construct loads [or creates] the file
+	 *
+	 * @param string $file
+	 */
+    function __construct($file)
+    {
+        $this->file = $file;
+
+        try {
+            // create file if not exist
+            if (!file_exists($this->file)) {
+                if (!$this->handle = fopen($this->file, 'w')) {
+                    throw new Exception('Cannot create file');
+                }
+                flock($this->handle, LOCK_EX);
+                fwrite(
+                    $this->handle,
+                    '# FlatFileCRUD by Lawrence Cherone'.PHP_EOL
+                );
+                flock($this->handle, LOCK_UN);
+                fclose($this->handle);
+            }
+
+            //check file is readable
+            if (!is_readable($this->file))  {
+                throw new Exception('File is not readable');
+            }
+
+            //check file is writable
+            if (!is_writable($this->file)) {
+                throw new Exception('File is not writable');
+            }
+
+        } catch (Exception $e) {
+            exit('Error: '.$e->getMessage());
+        }
+    }
+
+    /**
+	 * Create or update an entry in the .htaccess file
+	 *
+	 * @param string $data
+	 * @param string $delim
+	 */
+    function create($delim = '#', $data = PHP_EOL)
+    {
+        //first check for existing then update
+        if($this->read($delim)) {
+            $this->update($delim, $data);
+        } else {
+            //create new entry
+            file_put_contents(
+                $this->file,
+                PHP_EOL.'# '.$delim.PHP_EOL.$data.PHP_EOL.'# \\'.$delim.PHP_EOL,
+                FILE_APPEND
+            );
+        }
+    }
+
+    /**
+	 * Read entry from .htaccess file
+	 *
+	 * @param string $delim
+	 * @return mixed (bool|string)
+	 */
+    function read($delim = '#')
+    {
+        $file = file_get_contents($this->file);
+
+        $delim = preg_quote($delim);
+        if (preg_match("/#\s$delim\s(.*?)\s#\s\\\\$delim/s", $file, $matches)) {
+            return trim($matches[1]);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+	 * Update entry in .htaccess file
+	 *
+	 * @param string $data
+	 * @param string $delim
+	 */
+    function update($delim = '#', $data = PHP_EOL)
+    {
+        $data = str_replace(
+            array('$1','$2','$3','$4','$5'),
+            array('\$1','\$2','\$3','\$4','\$5'),
+            $data
+        );
+
+        $delim = preg_quote($delim);
+
+        file_put_contents(
+            $this->file,
+            trim(
+                preg_replace(
+                    "/#\s$delim\s(.*?)\s#\s\\\\$delim/s",
+                    '# '.$delim.PHP_EOL.$data.PHP_EOL.'# \\'.$delim,
+                    file_get_contents($this->file)
+                )
+            )
+        );
+    }
+
+    /**
+	 * Delete entry from .htaccess file
+	 *
+	 * @param string $delim
+	 * @return bool
+	 */
+    function delete($delim = '#')
+    {
+        $file = file_get_contents($this->file);
+
+        $delim = preg_quote($delim);
+        if (preg_match("/#\s$delim\s(.*?)\s#\s\\\\$delim/s", $file, $matches)) {
+            file_put_contents(
+                $this->file,
+                str_replace(PHP_EOL.$matches[0].PHP_EOL, '', $file)
+            );
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function dump()
+    {
+        return file_get_contents($this->file);
+    }
+
+    function drop()
+    {
+        return file_put_contents($this->file, '');
+    }
+
+}
